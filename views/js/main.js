5993721236f149c1e3eee605d25c009614a4d6ed
Dwayne Thomas <thomas.dwayne@gmail.com>
2015-12-20 17:55:29 -0800

    removing variable assignments from loop.

--- views/js/main.js
+++ views/js/main.js
@@ -448,12 +448,15 @@ var resizePizzas = function(size) {
     return dx;
   }
 
+
+
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+  // DT Optimization, removed variable assignments from loop.
+    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[0], size);
+    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[0].offsetWidth + dx) + 'px';
+      for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+        document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
